06b836e72e0b0c48136548fe62f703d1bab3f442
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2020-11-25 15:46:53 -0300

    ignore flake8 rule

--- hive_metastore_client/hive_mestastore_client_connector.py
+++ hive_metastore_client/hive_mestastore_client_connector.py
@@ -2,7 +2,7 @@
 from thrift.protocol import TBinaryProtocol
 from thrift.transport import TSocket, TTransport
 
-from hive_metastore_client.clients.thrift_hive_metastore_client.ThriftHiveMetastore import (  # type: ignore
+from hive_metastore_client.clients.thrift_hive_metastore_client.ThriftHiveMetastore import (  # type: ignore # noqa: E501
     Client as ThriftClient,
 )
 
