2941486ecee3667d956dbd03ed417f81bbccb77b
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2020-11-25 11:43:40 -0300

    Apply lint on test files

--- tests/unit/hive_metastore_client/conftest.py
+++ tests/unit/hive_metastore_client/conftest.py
@@ -3,10 +3,12 @@ from unittest import mock
 import pytest
 
 from hive_metastore_client.hive_mestastore_client import HiveMetastoreClient
-from hive_metastore_client.hive_mestastore_client_connector import HiveMetastoreClientConnector
+from hive_metastore_client.hive_mestastore_client_connector import (
+    HiveMetastoreClientConnector,
+)
 
 
 @pytest.fixture
-@mock.patch.object(HiveMetastoreClientConnector, '_init_protocol')
+@mock.patch.object(HiveMetastoreClientConnector, "_init_protocol")
 def hive_metastore_client(mocked__init_protocol) -> HiveMetastoreClient:
-    return HiveMetastoreClient(host='__HOST__')
+    return HiveMetastoreClient(host="__HOST__")
--- tests/unit/hive_metastore_client/test_hive_mestastore_client_connector.py
+++ tests/unit/hive_metastore_client/test_hive_mestastore_client_connector.py
@@ -1,34 +1,35 @@
 from unittest import mock
 from unittest.mock import Mock
 
-from thrift.protocol import TBinaryProtocol
-
-from hive_metastore_client.hive_mestastore_client_connector import HiveMetastoreClientConnector
+from hive_metastore_client.hive_mestastore_client_connector import (
+    HiveMetastoreClientConnector,
+)
 
 
 class TestHiveMetastoreClientConnector:
-
     @mock.patch("hive_metastore_client.hive_mestastore_client_connector.TSocket")
     @mock.patch("hive_metastore_client.hive_mestastore_client_connector.TTransport")
-    @mock.patch("hive_metastore_client.hive_mestastore_client_connector.TBinaryProtocol")
-    def test__init_protocol(self,
-                            mocked_tbinaryp,
-                            mocked_ttransport,
-                            mocked_tsocket,
-                            hive_metastore_client):
-            # arrange
-            mocked_transport = Mock()
-            mocked_ttransport.TBufferedTransport.return_value = mocked_transport
+    @mock.patch(
+        "hive_metastore_client.hive_mestastore_client_connector.TBinaryProtocol"
+    )
+    def test__init_protocol(
+        self, mocked_tbinaryp, mocked_ttransport, mocked_tsocket, hive_metastore_client
+    ):
+        # arrange
+        mocked_transport = Mock()
+        mocked_ttransport.TBufferedTransport.return_value = mocked_transport
 
-            expected_return = Mock()
-            mocked_tbinaryp.TBinaryProtocol.return_value = expected_return
+        expected_return = Mock()
+        mocked_tbinaryp.TBinaryProtocol.return_value = expected_return
 
-            # act
-            returned_value = hive_metastore_client._init_protocol(host='111.222.333', port=1234)
+        # act
+        returned_value = hive_metastore_client._init_protocol(
+            host="111.222.333", port=1234
+        )
 
-            # assert
-            assert expected_return == returned_value
-            mocked_tbinaryp.TBinaryProtocol.assert_called_once_with(mocked_transport)
+        # assert
+        assert expected_return == returned_value
+        mocked_tbinaryp.TBinaryProtocol.assert_called_once_with(mocked_transport)
 
     def test_open(self, hive_metastore_client):
         # arrange
@@ -53,19 +54,19 @@ class TestHiveMetastoreClientConnector:
         # assert
         hive_metastore_client._oprot.trans.close.assert_called_once_with()
 
-    @mock.patch.object(HiveMetastoreClientConnector, 'open')
+    @mock.patch.object(HiveMetastoreClientConnector, "open")
     def test__enter__(self, mocked_open, hive_metastore_client):
         # act
-        with hive_metastore_client as c:
+        with hive_metastore_client:
             pass
 
         # assert
         mocked_open.assert_called_once_with()
 
-    @mock.patch.object(HiveMetastoreClientConnector, 'close')
+    @mock.patch.object(HiveMetastoreClientConnector, "close")
     def test__exit__(self, mocked_close, hive_metastore_client):
         # act
-        with hive_metastore_client as c:
+        with hive_metastore_client:
             pass
 
         # assert
