0325ace31631f806eb15a14af2f401145676d385
Juliana Freire <5251778+jufreire@users.noreply.github.com>
2020-12-02 16:15:27 -0300

    adding method to add columns to a table (#18)

--- examples/create_database.py
+++ examples/create_database.py
@@ -1,12 +1,18 @@
+"""
+    Use the builders for creating Database thrift object.
+    Some arguments are optional when creating a thrift object.
+    Check Builder constructor for more information.
+"""
+
 from hive_metastore_client.builders.database_builder import DatabaseBuilder
 from hive_metastore_client.hive_mestastore_client import HiveMetastoreClient
 
 HIVE_HOST = "<ADD_HIVE_HOST_HERE>"
 HIVE_PORT = 9083
 
+# Creating database object using builder
+database = DatabaseBuilder("database_name").build()
+
 with HiveMetastoreClient(HIVE_HOST, HIVE_PORT) as hive_client:
-
-    database_builder = DatabaseBuilder("database_name")
-    database = database_builder.build()
-
+    # Creating new database from thrift table object
     hive_client.create_database(database)
