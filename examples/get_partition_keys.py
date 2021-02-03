8c893f9aec1698aa515670e1c21612551208beda
felipemiquelim <felipemiquelim@gmail.com>
2021-02-03 17:01:13 -0300

    Adding get_partition_keys and key_names methods

---
+++ examples/get_partition_keys.py
@@ -0,0 +1,11 @@
+from hive_metastore_client import HiveMetastoreClient
+
+HIVE_HOST = "<ADD_HIVE_HOST_HERE>"
+HIVE_PORT = 9083
+
+DATABASE_NAME = "database_name"
+TABLE_NAME = "table_name"
+
+with HiveMetastoreClient(HIVE_HOST, HIVE_PORT) as hive_client:
+    # Retrieving the partition keys via table schema
+    hive_client.get_partition_keys(DATABASE_NAME, TABLE_NAME)
---
+++ examples/get_partition_keys_names.py
@@ -0,0 +1,11 @@
+from hive_metastore_client import HiveMetastoreClient
+
+HIVE_HOST = "<ADD_HIVE_HOST_HERE>"
+HIVE_PORT = 9083
+
+DATABASE_NAME = "database_name"
+TABLE_NAME = "table_name"
+
+with HiveMetastoreClient(HIVE_HOST, HIVE_PORT) as hive_client:
+    # Retrieving the partition keys names via table schema
+    hive_client.get_partition_keys_names(DATABASE_NAME, TABLE_NAME)
