04a1fcc7800c256d2dd94c3697d73c98b6780032
Felipe Miquelim <felipe.miquelim@quintoandar.com.br>
2021-02-24 11:20:56 -0300

    [DAE-122] Release 1.0.5 (#51)

---
+++ examples/drop_partition.py
@@ -0,0 +1,18 @@
+from hive_metastore_client.builders import PartitionBuilder
+from hive_metastore_client import HiveMetastoreClient
+
+HIVE_HOST = "<ADD_HIVE_HOST_HERE>"
+HIVE_PORT = 9083
+
+DATABASE_NAME = "database_name"
+TABLE_NAME = "table_name"
+
+# Partitions should be provided as list
+# Values should be passed in the same hierarchical order of the partitions
+partition_values = ["2020", "12", "13"]
+
+with HiveMetastoreClient(HIVE_HOST, HIVE_PORT) as hive_client:
+    # Dropping respective partition from metastore but keeping its data
+    hive_client.drop_partition(
+        DATABASE_NAME, TABLE_NAME, partition_values, deleteData=False
+    )
